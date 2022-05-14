9d4ae8ee1de1dceb99057ce286117b74d5684991
Hirutsuki <61302403+Hirutsuki@users.noreply.github.com>
2022-05-15 01:04:57 +0800

    removed redundant code

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,7 +1,5 @@
-const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
